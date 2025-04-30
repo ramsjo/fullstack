b6e445cbde96bf8ff893c8537c01849f7be8b64f
ramsjo <99391151+ramsjo@users.noreply.github.com>
2025-04-30 10:21:19 +0530

    Update server.js

--- fullstack/server/server.js
+++ fullstack/server/server.js
@@ -11,12 +11,12 @@ const db = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-app.get('/api/todos', async (req, res) = {
+app.get('/api/todos', async (req, res) => {
   const result = await db.query('SELECT  FROM todos');
   res.json(result.rows);
 });
 
-app.post('/api/todos', async (req, res) = {
+app.post('/api/todos', async (req, res) => {
   const { text } = req.body;
   await db.query('INSERT INTO todos (text) VALUES ($1)', [text]);
   res.sendStatus(201);
